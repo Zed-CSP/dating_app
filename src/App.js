82f0daf7c034742cc3f8df65658a68a49a5127a1
Christopher Peret <cperet89@gmail.com>
2023-09-15 17:27:25 -0700

    nav init

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
+import Nav from './Nav';
 
 function App() {
   return (
     <div className="App">
       <h1>Tinder Clone App </h1>
+      {/* Header/Nav */}
+      <Nav />
+      {/* Tinder Cards */}
+      {/* Buttons below tinder cards */}
+      {/* Chat screen */}
+      {/* Individual chat screen */}
     </div>
   );
 }
---
+++ src/Nav.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+function Nav() {
+  return (
+    <nav>
+        <h2>I am the nav</h2>
+    </nav>
+  )
+}
+
+export default Nav
